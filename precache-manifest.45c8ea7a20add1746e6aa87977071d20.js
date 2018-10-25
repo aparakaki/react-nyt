f4d405cbbadd216734977ff83025e4f3ec3eb4e8
Ana <aparakaki17@gmail.com>
2018-10-24 22:09:59 -0700

    Updates

---
+++ precache-manifest.45c8ea7a20add1746e6aa87977071d20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-nyt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df64ead83c71a52a6508",
+    "url": "/react-nyt/static/js/runtime~main.df64ead8.js"
+  },
+  {
+    "revision": "72c8f30c4e11ff108599",
+    "url": "/react-nyt/static/js/main.72c8f30c.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/react-nyt/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "72c8f30c4e11ff108599",
+    "url": "/react-nyt/static/css/main.8b2f29ec.chunk.css"
+  },
+  {
+    "revision": "eadd71f9929df842307d3f675ada5684",
+    "url": "/react-nyt/index.html"
+  }
+];
